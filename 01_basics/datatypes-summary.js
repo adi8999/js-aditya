3fa3fa6d0b86e2bb8485478f0a9fb40642e10fb6
Aditya Shukla <168159454+adi8999@users.noreply.github.com>
2025-04-13 12:53:38 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive datatype:
+
+ // 7 Types : String, Number,Boolean,Null,Undefined,Symbol,BigInt.
+
+ const score = 100
+ const scoreValue= 100.3
+
+ const isLoggedIn = false
+ const outsideTemp = null
+
+ let userEmail;
+  const id =Symbol ('123');
+  const anotherId =Symbol('123');
+ // console.log(id===anotherId);
+  
+  //const bigNumber = 357486586768n
+  
+
+ // Non - Primitive or Reference:
+
+ // Array , Objects , Functions.
+
+ const heroes =["ironman ", "superman", "Thanos"]
+ let myObj = {
+    name:"aditya",
+    age :22,
+
+  }
+
+  const myFunction = function(){
+    console.log("Hello World");
+
+  }
+  console.log(typeof outsideTemp);
+
