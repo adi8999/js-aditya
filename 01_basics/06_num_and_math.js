e1a572ce207b8a2cb2be8b6feeaa11d49287cad5
Aditya Shukla <168159454+adi8999@users.noreply.github.com>
2025-04-19 12:28:37 +0000

    number and math in js

--- 01_basics/06_num_and_math.js
+++ 01_basics/06_num_and_math.js
@@ -6,3 +6,10 @@ console.log(balance);
 
 console.log(balance.toString());
 console.log(balance.toFixed(2));
+
+const otherNumber =33.4455;
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000
+
+console.log(hundreds.toLocaleString());
