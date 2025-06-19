cd7e17507db788e06cd030f4855d8eefb650bc7c
Aditya Shukla <adityapandit322@gmail.com>
2025-06-19 16:05:06 +0530

    num and math

--- 01_basics/06_math_and_num.js
+++ 01_basics/06_math_and_num.js
@@ -12,4 +12,24 @@ console.log(otherNumber.toPrecision(3));
 
 const hundreds = 100000
 
-console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString( 'en-IN'));
+
+// +++++++++++ Math ++++++++++++
+
+//console.log(Math);
+//console.log(Math.abs(-3));  // it gives positive number //
+//console.log(Math.round(4.6)); 
+//console.log(Math.ceil(4.5));
+//console.log(Math.floor(4.1));
+//console.log(Math.min(2,3,4,5));
+
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+console.log(Math.floor(Math.random()* 10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() *(max - min +1)) + min)
+
+
